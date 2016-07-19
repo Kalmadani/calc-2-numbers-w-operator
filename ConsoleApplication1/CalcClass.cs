5725608b908c48dc404600b24dd29d4ec0a1ffad
Kal <khalid.almadani@agilethought.com>
2016-07-19 12:57:32 -0400

    calc 2 numbers w operator

---
+++ ConsoleApplication1/CalcClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class CalcClass
+    {
+        public void DetermineOperator(string userOperator, int userFirstNumber, int userSecondNumber)
+        {
+
+            if (userOperator == "+")
+            {
+                Add(userFirstNumber, userSecondNumber);
+            }
+            if (userOperator == "-")
+            {
+                Subtract(userFirstNumber, userSecondNumber);
+            }
+
+            //switch (userOperator)
+            //{
+            //    case "+":
+            //        Add(userFirstNumber, userSecondNumber);
+            //        break;
+            //    case "-":
+            //        Subtract(userFirstNumber, userSecondNumber);
+            //        break;
+            //}
+        }
+
+            private void Subtract(int subFirstNumber, int subSecondNumber)
+        {
+            int answer = subFirstNumber - subSecondNumber;
+            Console.WriteLine("The answer is " + answer);
+            Console.ReadKey();
+
+        }
+
+        private void Add (int addFirstNumber, int addSecondNumber)
+        {
+            int answer = addFirstNumber + addSecondNumber;
+            Console.WriteLine("The answer is " + answer);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose any number");
+            int userFirstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an operator");
+            string userOperator = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Choose a second number");
+            int userSecondNumber = Convert.ToInt32(Console.ReadLine());
+
+            CalcClass calcClass = new CalcClass();
+            calcClass.DetermineOperator(userOperator, userFirstNumber, userSecondNumber);
+        }
+    }
+}
